044ba3381ebbd8891b35b3864543011b83665fcf
Maahishah <76730966+Maahishah@users.noreply.github.com>
2023-02-28 12:35:23 -1200

    Add files via upload

---
+++ PRO-C108-Teacher-Bolierplate-main/count_fingers.py
@@ -0,0 +1,16 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, image = cap.read()
+
+    cv2.imshow("Media Controller", image)
+
+    key = cv2.waitKey(1)
+    if key == 32:
+        break
+
+cv2.destroyAllWindows()
+
